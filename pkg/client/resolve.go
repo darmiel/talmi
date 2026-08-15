bbf554a138261a80cef657a932a5de3c60b3b0c4
darmiel <71837281+darmiel@users.noreply.github.com>
2026-08-15 16:13:23 +0200

    feat: added `providers` and `resolve` routes to client
    Signed-off-by: darmiel <71837281+darmiel@users.noreply.github.com>

---
+++ pkg/client/resolve.go
@@ -0,0 +1,23 @@
+package client
+
+import (
+	"context"
+
+	"github.com/darmiel/talmi/internal/api"
+	"github.com/darmiel/talmi/internal/resolver"
+)
+
+type resolveRequestBody struct {
+	Resources []ResourceRequest `json:"resources"`
+}
+
+func (c *Client) Resolve(
+	ctx context.Context,
+	requests []ResourceRequest,
+) ([]resolver.RequestResolution, string, error) {
+	var out []resolver.RequestResolution
+	correlationID, err := c.post(ctx, c.url().setPath(api.ResolveRoute).build(), resolveRequestBody{
+		Resources: requests,
+	}, &out)
+	return out, correlationID, err
+}
